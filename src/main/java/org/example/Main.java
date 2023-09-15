82dd7210724d2e05a73aec37c00407e95e183779
Arman I <88910492+Kotik112@users.noreply.github.com>
2023-09-15 22:34:16 +0200

    Added the data file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
+/*
+* TODO: Läs av textfilen data.txt och läs av alla värden. Rökna och printa ut medelvärdet av alla värden.
+* */
+
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		// Skriv Kod här.
+		// Läs av koden
+	}
 }
